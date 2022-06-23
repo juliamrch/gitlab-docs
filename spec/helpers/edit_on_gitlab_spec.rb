1422aaf82e1b9eb4175eb6e2d368578677853b06
Brian Williams <bwilliams@gitlab.com>
2022-06-23 07:47:22 -0500

    Refactor edit_on_gitlab helper
    Rather than having a bunch of case statements with URLs, this changes
    the `edit_on_gitlab` helper so that it builds urls from a project path,
    a branch name, and a docs file path. This is easier to maintain since
    it's a data-driven approach. The PRODUCT_REPOS data can be edited
    without changing the program logic.

--- spec/helpers/edit_on_gitlab_spec.rb
+++ spec/helpers/edit_on_gitlab_spec.rb
@@ -20,17 +20,17 @@ RSpec.describe Nanoc::Helpers::EditOnGitLab do
     using RSpec::Parameterized::TableSyntax
 
     where(:identifier, :editor, :expected_url) do
-      "/omnibus/index.md"  | :simple | "https://gitlab.com/gitlab-org/omnibus-gitlab/blob/master/doc/index.md"
+      "/omnibus/index.md"  | :simple | "https://gitlab.com/gitlab-org/omnibus-gitlab/-/blob/master/doc/index.md"
       "/omnibus/index.md"  | :webide | "https://gitlab.com/-/ide/project/gitlab-org/omnibus-gitlab/edit/master/-/doc/index.md"
-      "/runner/index.md"   | :simple | "https://gitlab.com/gitlab-org/gitlab-runner/blob/main/docs/index.md"
+      "/runner/index.md"   | :simple | "https://gitlab.com/gitlab-org/gitlab-runner/-/blob/main/docs/index.md"
       "/runner/index.md"   | :webide | "https://gitlab.com/-/ide/project/gitlab-org/gitlab-runner/edit/main/-/docs/index.md"
-      "/charts/index.md"   | :simple | "https://gitlab.com/gitlab-org/charts/gitlab/blob/master/doc/index.md"
+      "/charts/index.md"   | :simple | "https://gitlab.com/gitlab-org/charts/gitlab/-/blob/master/doc/index.md"
       "/charts/index.md"   | :webide | "https://gitlab.com/-/ide/project/gitlab-org/charts/gitlab/edit/master/-/doc/index.md"
-      "/operator/index.md" | :simple | "https://gitlab.com/gitlab-org/cloud-native/gitlab-operator/blob/master/doc/index.md"
+      "/operator/index.md" | :simple | "https://gitlab.com/gitlab-org/cloud-native/gitlab-operator/-/blob/master/doc/index.md"
       "/operator/index.md" | :webide | "https://gitlab.com/-/ide/project/gitlab-org/cloud-native/gitlab-operator/edit/master/-/doc/index.md"
-      "/ee/user/ssh.md"    | :simple | "https://gitlab.com/gitlab-org/gitlab/blob/master/doc/user/ssh.md"
+      "/ee/user/ssh.md"    | :simple | "https://gitlab.com/gitlab-org/gitlab/-/blob/master/doc/user/ssh.md"
       "/ee/user/ssh.md"    | :webide | "https://gitlab.com/-/ide/project/gitlab-org/gitlab/edit/master/-/doc/user/ssh.md"
-      "/content/404.html"  | :simple | "https://gitlab.com/gitlab-org/gitlab-docs/blob/main/content/404.html"
+      "/content/404.html"  | :simple | "https://gitlab.com/gitlab-org/gitlab-docs/-/blob/main/content/404.html"
       "/content/404.html"  | :webide | "https://gitlab.com/-/ide/project/gitlab-org/gitlab-docs/edit/main/-/content/404.html"
     end
 
