0430fea527c4c81ef4122536faf07d3fb6d2ca4b
Sarah German <sgerman@gitlab.com>
2022-07-13 11:54:03 +0000

    Enable filtering the Deprecations list with URL parameters

--- spec/frontend/deprecations/deprecation_filters_spec.js
+++ spec/frontend/deprecations/deprecation_filters_spec.js
@@ -5,12 +5,32 @@
 import { mount } from '@vue/test-utils';
 import DeprecationFilters from '../../../content/frontend/deprecations/components/deprecation_filters.vue';
 
-const propsData = { showAllText: 'Show all', milestonesList: [] };
+const propsData = { showAllText: 'Show all', milestonesList: [{ value: '160', text: '16.0' }] };
 const removalsFilterSelector = '[data-testid="removal-milestone-filter"]';
 
 describe('component: Deprecations Filter', () => {
   it('Filter is visible', () => {
     const wrapper = mount(DeprecationFilters, { propsData });
     expect(wrapper.find(removalsFilterSelector).isVisible()).toBe(true);
+  });
+
+  it('Validates a URL parameter', () => {
+    const location = {
+      ...window.location,
+      search: '?removal_milestone=16.0',
+      toString: () => {
+        return 'http://localhost/ee/update/deprecations.html';
+      },
+    };
+    Object.defineProperty(window, 'location', {
+      writable: true,
+      value: location,
+    });
+
+    const searchParams = new URLSearchParams(window.location.search);
+    const versionValue = searchParams.get('removal_milestone').replace(/\./g, '');
+
+    const wrapper = mount(DeprecationFilters, { propsData });
+    expect(wrapper.vm.isValidVersion(versionValue)).toBe(true);
   });
 });
