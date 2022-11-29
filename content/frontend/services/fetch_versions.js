eca36c611998cebd65fa612f8a387b35438c77dd
Sarah German <sgerman@gitlab.com>
2022-11-29 14:59:38 -0600

    Add archives redirect handler to 404 page

--- content/frontend/services/fetch_versions.js
+++ content/frontend/services/fetch_versions.js
@@ -1,9 +1,16 @@
 /* eslint-disable no-console */
 
+import { compareVersions } from 'compare-versions';
+
 const DOCS_VERSIONS_ENDPOINT = 'https://docs.gitlab.com/versions.json';
 const DOCS_IMAGES_ENDPOINT =
   'https://gitlab.com/api/v4/projects/1794617/registry/repositories/631635/tags?per_page=100';
 
+/**
+ * Fetch a list of versions available on docs.gitlab.com.
+ *
+ * @returns Array
+ */
 export function getVersions() {
   return fetch(DOCS_VERSIONS_ENDPOINT)
     .then((response) => response.json())
@@ -13,6 +20,11 @@ export function getVersions() {
     .catch((error) => console.error(error));
 }
 
+/**
+ * Fetch a list of archived versions available as container images.
+ *
+ * @returns Array
+ */
 export function getArchiveImages() {
   return fetch(DOCS_IMAGES_ENDPOINT)
     .then((response) => response.json())
@@ -21,4 +33,24 @@ export function getArchiveImages() {
       return data.filter((object) => !Number.isNaN(Number(object.name))).reverse();
     })
     .catch((error) => console.error(error));
+}
+
+/**
+ * Fetch a list of versions available on the archives site.
+ *
+ * @returns Array
+ */
+export async function getArchivesVersions() {
+  const onlineVersions = await getVersions();
+  const archiveImages = await getArchiveImages();
+
+  const oldestSupportedMinor = onlineVersions.last_major[1].split('.')[0];
+  const oldestCurrentMinor = onlineVersions.last_minor[1];
+
+  return archiveImages
+    .map((object) => object.name)
+    .filter(
+      (v) =>
+        compareVersions(v, oldestSupportedMinor) >= 0 && compareVersions(v, oldestCurrentMinor) < 0,
+    );
 }
