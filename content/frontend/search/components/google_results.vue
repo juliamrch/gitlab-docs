f15693455391c9d5403bc91437a055246d0447bf
Sarah German <sgerman@gitlab.com>
2023-02-17 10:23:10 -0600

    Add search filtering options

--- content/frontend/search/components/google_results.vue
+++ content/frontend/search/components/google_results.vue
@@ -7,11 +7,14 @@ import {
   GlSafeHtmlDirective as SafeHtml,
   GlPagination,
 } from '@gitlab/ui';
+import isEqual from 'lodash.isequal';
 import { getSearchQueryFromURL, updateURLParams } from '../search_helpers';
 import { GPS_ENDPOINT, GPS_ID } from '../../services/google_search_api';
+import SearchFilters from './search_filters.vue';
 
 export default {
   components: {
+    SearchFilters,
     GlSearchBoxByClick,
     GlLink,
     GlLoadingIcon,
@@ -30,6 +33,7 @@ export default {
       pageNumber: 1,
       response: {},
       results: [],
+      activeFilters: [],
     };
   },
   computed: {
@@ -39,11 +43,14 @@ export default {
       return `Showing ${startIndex}-${end} of ${this.response.searchInformation.formattedTotalResults} results`;
     },
     noResults() {
-      return this.submitted && !this.loading && !this.results.length && !this.error;
+      return this.query && !this.loading && !this.results.length && !this.error;
     },
     showPager() {
       return (
-        this.submitted && this.response.searchInformation.totalResults > this.MAX_RESULTS_PER_PAGE
+        this.submitted &&
+        this.results.length &&
+        this.response.searchInformation.totalResults > this.MAX_RESULTS_PER_PAGE &&
+        !this.loading
       );
     },
     pagerMaxItems() {
@@ -57,22 +64,28 @@ export default {
   },
   mounted() {
     if (this.query) {
-      this.search(this.query);
+      this.search(this.query, this.activeFilters);
     }
   },
   methods: {
     cleanTitle(title) {
       return title.replace(' | GitLab', '');
     },
-    async fetchGoogleResults() {
+    async fetchGoogleResults(filters) {
       let data = {};
+
+      // Construct the query string for additional filters if needed.
+      const filterQuery = filters.length
+        ? `+more:pagemap:metatags-gitlab-docs-section:${filters.join(',')}`
+        : '';
+
       try {
         const response = await fetch(
           GPS_ENDPOINT +
             new URLSearchParams({
               key: GOOGLE_SEARCH_KEY,
               cx: GPS_ID,
-              q: this.query,
+              q: `${this.query}${filterQuery}`.replaceAll(' ', '*'),
               start: (this.pageNumber - 1) * this.MAX_RESULTS_PER_PAGE + 1,
             }),
         );
@@ -85,29 +98,36 @@ export default {
     },
     handleError(error) {
       this.error = true;
+      this.loading = false;
       throw new Error(`Error code ${error.code}: ${error.message}`);
     },
-    onSubmit() {
-      if (this.query) {
-        this.search(this.query);
-      }
-    },
-    async search(query) {
-      this.query = query;
+    async search(query, filters = []) {
       this.results = [];
+      if (!query) return;
 
-      this.loading = true;
-      this.response = await this.fetchGoogleResults();
-      this.results = this.response.items ? this.response.items : [];
-      this.loading = false;
-      this.submitted = true;
-      updateURLParams(this.query);
+      // If the query or filters changed, return to page 1 of results.
+      if (query !== this.query || !isEqual(filters, this.activeFilters)) this.pageNumber = 1;
 
-      // Add a relative link to each result object.
-      this.results = this.results.map((obj) => ({
-        ...obj,
-        relativeLink: obj.link.replace('https://docs.gitlab.com/', '/'),
-      }));
+      this.query = query;
+      this.activeFilters = filters;
+
+      try {
+        this.loading = true;
+        this.response = await this.fetchGoogleResults(filters);
+        this.results = this.response.items ? this.response.items : [];
+      } catch (error) {
+        this.handleError(error);
+      } finally {
+        this.loading = false;
+        this.submitted = true;
+        updateURLParams(this.query);
+
+        // Add a relative link to each result object.
+        this.results = this.results.map((obj) => ({
+          ...obj,
+          relativeLink: obj.link.replace('https://docs.gitlab.com/', '/'),
+        }));
+      }
     },
   },
 };
@@ -116,46 +136,62 @@ export default {
 <template>
   <div class="google-search gl-mb-9">
     <h1>Search</h1>
-    <gl-search-box-by-click v-model="query" :value="query" @submit="onSubmit" />
-    <div v-if="results.length" class="gl-font-sm gl-mb-5">
-      {{ resultSummary }}
+    <div class="gl-h-11 gl-mb-5">
+      <gl-search-box-by-click v-model="query" :value="query" @submit="search(query)" />
+      <div v-if="results.length" class="gl-font-sm gl-mb-5 gl-ml-1">
+        {{ resultSummary }}
+      </div>
     </div>
 
-    <gl-loading-icon v-if="loading" size="lg" class="gl-mt-5" />
+    <div class="results-container gl-lg-display-flex">
+      <div v-if="submitted" class="results-sidebar gl-mb-5 lg-w-20p">
+        <h2 class="gl-mt-0! gl-mb-5!">Filter results</h2>
+        <search-filters @filteredSearch="(filters) => search(query, filters)" />
+      </div>
 
-    <ul
-      v-if="results.length"
-      class="gl-list-style-none gl-pl-2 gl-max-w-80"
-      data-testid="search-results"
-    >
-      <li v-for="result in results" :key="result.cacheId" class="gl-mb-5!">
-        <gl-link
-          :href="`${result.relativeLink}`"
-          class="gl-font-lg gl-border-bottom-0! gl-hover-text-decoration-underline:hover gl-mb-2"
-          >{{ cleanTitle(result.title) }}</gl-link
-        >
-        <p v-safe-html="result.htmlSnippet" class="result-snippet"></p>
-      </li>
-    </ul>
+      <div class="lg-w-70p">
+        <gl-loading-icon v-if="loading" size="lg" class="gl-mt-5 gl-text-center" />
 
-    <gl-pagination
-      v-if="showPager"
-      v-model="pageNumber"
-      :per-page="MAX_RESULTS_PER_PAGE"
-      :total-items="pagerMaxItems"
-      class="gl-mt-9"
-      @input="search(query)"
-    />
+        <ul v-if="results.length" class="gl-list-style-none gl-pl-2" data-testid="search-results">
+          <li v-for="result in results" :key="result.cacheId" class="gl-mb-5!">
+            <gl-link
+              :href="`${result.relativeLink}`"
+              class="gl-font-lg gl-border-bottom-0! gl-hover-text-decoration-underline:hover gl-mb-2"
+              >{{ cleanTitle(result.title) }}
+            </gl-link>
+            <p v-safe-html="result.htmlSnippet" class="result-snippet"></p>
+          </li>
+        </ul>
 
-    <p v-if="noResults" class="gl-py-5">No results found.</p>
-    <p v-if="error" class="gl-py-5" data-testid="search-error">
-      Error fetching results. Please try again later.
-    </p>
+        <gl-pagination
+          v-if="showPager"
+          v-model="pageNumber"
+          :per-page="MAX_RESULTS_PER_PAGE"
+          :total-items="pagerMaxItems"
+          class="gl-mt-9"
+          @input="search(query, activeFilters)"
+        />
+
+        <p v-if="noResults" class="gl-py-5">
+          No results found. Try adjusting your search terms, or searching the
+          <gl-link class="gl-font-base" href="https://forum.gitlab.com/">community forum</gl-link>.
+        </p>
+        <p v-if="error" class="gl-py-5" data-testid="search-error">
+          Error fetching results. Please try again later.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style>
+html {
+  overflow-y: scroll;
+}
 .result-snippet {
   font-size: 0.875rem;
+}
+.results-sidebar h2 {
+  font-size: 1.5rem;
 }
 </style>
