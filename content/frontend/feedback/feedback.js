a8af88ade5ef675d832ba9ce28bda1f30b525695
Sarah German <sgerman@gitlab.com>
2023-03-20 21:08:51 +0000

    Remove Disqus commenting

--- content/frontend/feedback/feedback.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const hasCommentAnchor = window.location.hash.includes('#comment-');
-
-  if (hasCommentAnchor) {
-    window.loadDisqus();
-  }
-});
