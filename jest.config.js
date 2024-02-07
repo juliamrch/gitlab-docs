51fce115ad6b92a8dd1f0a9eec39b222431fc94b
Sarah German <sgerman@gitlab.com>
2024-02-07 22:42:42 +0000

    Remove Vue 3 testing and clean up peer dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,40 +1,12 @@
 const setupFilesAfterEnv = ['<rootDir>/spec/frontend/jest.overrides.js'];
-
-const USE_VUE_3 = process.env.VUE_VERSION === '3';
-const VUE_JEST_TRANSFORMER = USE_VUE_3 ? '@vue/vue3-jest' : '@vue/vue2-jest';
-
-const customModuleNameMappers = {};
-const extraJestConfig = {};
-
-if (USE_VUE_3) {
-  global.console.log('Using Vue.js 3');
-
-  setupFilesAfterEnv.unshift('<rootDir>/spec/frontend/jest_setup_vue3_compat.js');
-  Object.assign(customModuleNameMappers, {
-    '^vue$': '@vue/compat',
-    '^@vue/test-utils$': '@vue/test-utils-vue3',
-  });
-  Object.assign(extraJestConfig, {
-    globals: {
-      'vue-jest': {
-        compilerOptions: {
-          compatConfig: {
-            MODE: 2,
-          },
-        },
-      },
-    },
-  });
-}
+const VUE_JEST_TRANSFORMER = '@vue/vue2-jest';
 
 module.exports = {
-  ...extraJestConfig,
   testMatch: ['<rootDir>/spec/frontend/**/**/*_spec.js'],
   moduleFileExtensions: ['js', 'json', 'vue'],
   moduleNameMapper: {
     '^~(/.*)$': '<rootDir>/content/frontend$1',
     '\\.(css|less|sass|scss)$': '<rootDir>/spec/frontend/__mocks__/style_mock.js',
-    ...customModuleNameMappers,
   },
   cacheDirectory: '<rootDir>/tmp/cache/jest',
   restoreMocks: true,
